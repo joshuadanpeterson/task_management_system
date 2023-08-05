79b8cd2e87877433331ef48d64ea3bae21fa6e13
Josh Peterson <joshuadanpeterson@gmail.com>
2023-08-05 01:07:44 -0600

    updated forms.py and views.py

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Task
 
 # This class is used to create a user registration form.
 class CustomUserCreationForm(UserCreationForm):
@@ -9,3 +10,8 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = ['username', 'password1', 'password2']
 
+# This class is used to represent the Task model in form format.
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['user', 'title', 'description', 'due_date', 'due_time', 'status']
--- app/views.py
+++ app/views.py
@@ -1,20 +1,14 @@
 # This is the view for the login page. It will authenticate the user and log them in if the credentials are correct.
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect, get_object_or_404
+from app.models import Task
+from app.forms import TaskForm
 
 def user_login(request):
     """
     The function `user_login` handles user login requests, authenticates the user, and redirects to a
-    success page if the login is successful, otherwise it renders the login page with an error message.
-    
-    :param request: The `request` parameter is an object that represents the HTTP request made by the
-    user. It contains information about the request, such as the method used (e.g., GET or POST), the
-    URL being accessed, any data sent with the request, and more
-    :return: a rendered HTML template called 'login.html' if the request method is not "POST". If the
-    request method is "POST", it will either redirect to a success URL or render the 'login.html'
-    template with an error message if the login credentials are invalid.
-    """
+    success page if the login is successful, otherwise it renders the login page with an error message. 
+     """
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
