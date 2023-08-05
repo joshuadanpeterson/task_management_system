51ccf20f95b74d0af4451d20c67cb1c037f060e1
Josh Peterson <joshuadanpeterson@gmail.com>
2023-08-05 01:23:21 -0600

    Updated urls.py

--- task_management_system/urls.py
+++ task_management_system/urls.py
@@ -1,21 +1,25 @@
-"""task_management_system URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from app import views  # Assuming your app's name is 'app'
+from django.contrib.auth import views as auth_views  # Import built-in authentication views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # User registration or signup view
+    path('signup/', views.signup_view, name='signup'),
+    
+    # User login view
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    
+    # User logout view
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # Add paths for CRUD operations on tasks if you have views defined for them
+    # e.g., 
+    # path('tasks/', views.task_list, name='task_list'),
+    # path('tasks/create/', views.task_create, name='task_create'),
+    # path('tasks/<int:task_id>/', views.task_detail, name='task_detail'),
+    # path('tasks/<int:task_id>/edit/', views.task_edit, name='task_edit'),
+    # path('tasks/<int:task_id>/delete/', views.task_delete, name='task_delete'),
 ]
