7c733146db7585114d91c05a036615fa47a64fbc
Josh Peterson <joshuadanpeterson@gmail.com>
2023-08-19 21:37:51 -0600

    updated base, landing, login templates, urls.py, and added static folder

--- task_management_system/urls.py
+++ task_management_system/urls.py
@@ -18,6 +18,12 @@ urlpatterns = [
     # User logout view
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
+    # Password reset views
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
     # CRUD operations on tasks
     # e.g., 
     path('tasks/', views.task_list, name='task_list'),
