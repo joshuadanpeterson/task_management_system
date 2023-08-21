d8d68b442660087f87a771de23604ff1bab65461
Josh Peterson <joshuadanpeterson@gmail.com>
2023-08-21 01:41:32 -0600

    updated tests

--- app/tests/tests.py
+++ app/tests/tests.py
@@ -1,9 +1,37 @@
-# Testing the 'user_login' view in views.py
+# Inside app/tests/tests.py
+
+import pytest
+from django.contrib.auth.models import User
+from app.models import UserProfile, Task
 from django.urls import reverse
 from django.test import TestCase
-from django.contrib.auth.models import User
 
+# Model Tests
+class TestModels(TestCase):
+
+    def test_user_profile_creation(self):
+        user = User.objects.create_user(username='testuser', password='12345')
+        profile = UserProfile.objects.create(user=user)
+        self.assertEqual(profile.user, user)
+
+    def test_task_creation(self):
+        user = User.objects.create_user(username='testuser2', password='12345')
+        task = Task.objects.create(user=user, title="Test Task", description="Test Description", due_date="2023-08-04", due_time="13:00:00")
+        self.assertEqual(task.status, Task.STATUS_PENDING)
+
+    def test_task_status_update(self):
+        user = User.objects.create_user(username='testuser3', password='12345')
+        task = Task.objects.create(user=user, title="Test Task 2", description="Test Description 2", due_date="2023-08-05", due_time="14:00:00")
+        task.status = Task.STATUS_COMPLETED
+        task.save()
+        updated_task = Task.objects.get(id=task.id)
+        self.assertEqual(updated_task.status, Task.STATUS_COMPLETED)
+
+
+
+# View Tests
 class UserLoginViewTests(TestCase):
+
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='12345')
         self.login_url = reverse('login')  # Assuming the name of the login URL pattern is 'user_login'
@@ -20,10 +48,9 @@ class UserLoginViewTests(TestCase):
         print(response.content.decode())
 
 
-# Testing the 'task_create' view in views.py
-from app.models import Task
-
+# Tests for task creation
 class TaskCreateViewTests(TestCase):
+
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='12345')
         self.client.login(username='testuser', password='12345')
@@ -55,12 +82,14 @@ class TaskCreateViewTests(TestCase):
         self.assertEqual(response.status_code, 200)  # Expecting to stay on the same page
         self.assertEqual(Task.objects.count(), 0)  # No task should be created
 
-# Test the 'task_delete' view in views.py
+
+# Tests for task deletion
 class TaskDeleteViewTests(TestCase):
+   
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='12345')
         self.client.login(username='testuser', password='12345')
-        
+
         # Create a task
         self.task = Task.objects.create(
             user=self.user,
